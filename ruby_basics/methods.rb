5b4ced50b9dda41b9d8b89186827c082fb4a4661
Brad Graham <boosporch88888@gmail.com>
2020-10-04 03:21:47 +0000

    update

--- ruby_basics/methods.rb
+++ ruby_basics/methods.rb
@@ -1,119 +1,119 @@
 
-# # print me
+# print me
 
-# def print_me()
-#   puts "I'm printing within the method!"
-# end 
+def print_me()
+  puts "I'm printing within the method!"
+end 
 
-# print_me 
-
-
-# # print me part 2
-
-# def print_me()
-#   "I'm printing within the method!"
-# end 
-
-# puts print_me 
+print_me 
 
 
-# # Greeting Through Methods (Part 1)
+# print me part 2
 
-# def hello()
-#   "Hello"
-# end
+def print_me()
+  "I'm printing within the method!"
+end 
 
-# def world()
-#   "World"
-# end 
-
-# puts "#{hello} #{world}"
+puts print_me 
 
 
-# # Greeting Through Methods (Part 2)
+# Greeting Through Methods (Part 1)
 
-# def greeting()
-#   "#{hello} #{world}"
-# end 
+def hello()
+  "Hello"
+end
 
-# puts greeting
+def world()
+  "World"
+end 
 
-
-# # Make and Model
-
-# def car(make, model)
-#   puts "#{make} #{model}"
-# end 
-
-# car('Toyota', 'Corolla')
+puts "#{hello} #{world}"
 
 
-# # Day or Night?
+# Greeting Through Methods (Part 2)
 
-# def time_of_day(time)
-#   puts time ? "It's daytime!" : "It's nighttime!"
-# end 
+def greeting()
+  "#{hello} #{world}"
+end 
 
-# daylight = [true, false].sample
-
-# time_of_day(daylight)
+puts greeting
 
 
-# # Naming Animals
+# Make and Model
 
-# def dog(name)
-#   return name
-# end
+def car(make, model)
+  puts "#{make} #{model}"
+end 
 
-# def cat(name)
-#   return name
-# end
-
-# puts "The dog's name is #{dog('Spot')}."
-# puts "The cat's name is #{cat('Ginger')}."
+car('Toyota', 'Corolla')
 
 
-# # Name not Found
+# Day or Night?
 
-# def assign_name(name='Bob')
-#   name
-# end 
+def time_of_day(time)
+  puts time ? "It's daytime!" : "It's nighttime!"
+end 
 
-# puts assign_name('Kevin') == 'Kevin'
-# puts assign_name == 'Bob'
+daylight = [true, false].sample
 
-# # Mulitply the Sum 
-
-# def add(num1, num2)
-#   num1 + num2
-# end 
-
-# def multiply(num1, num2)
-#   num1 * num2
-# end 
-
-# puts add(2, 2) == 4
-# puts add(5, 4) == 9
-# puts multiply(add(2, 2), add(5, 4)) == 36
+time_of_day(daylight)
 
 
-# # random sentence
+# Naming Animals
 
-# def name(arr)
-#   arr.sample
-# end 
+def dog(name)
+  return name
+end
 
-# def activity(arr)
-#   arr.sample
-# end 
+def cat(name)
+  return name
+end
 
-# def sentence(name, activity)
-#   "#{name} went #{activity} today"
-# end 
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Ginger')}."
 
-# names = ['Dave', 'Sally', 'George', 'Jessica']
-# activities = ['walking', 'running', 'cycling']
 
-# puts sentence(name(names), activity(activities))
+# Name not Found
+
+def assign_name(name='Bob')
+  name
+end 
+
+puts assign_name('Kevin') == 'Kevin'
+puts assign_name == 'Bob'
+
+# Mulitply the Sum 
+
+def add(num1, num2)
+  num1 + num2
+end 
+
+def multiply(num1, num2)
+  num1 * num2
+end 
+
+puts add(2, 2) == 4
+puts add(5, 4) == 9
+puts multiply(add(2, 2), add(5, 4)) == 36
+
+
+# random sentence
+
+def name(arr)
+  arr.sample
+end 
+
+def activity(arr)
+  arr.sample
+end 
+
+def sentence(name, activity)
+  "#{name} went #{activity} today"
+end 
+
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+puts sentence(name(names), activity(activities))
 
 
