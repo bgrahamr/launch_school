128fc24b28532abdfb02bf6eca94a022090fcf7c
Brad Graham <boosporch88888@gmail.com>
2020-10-08 19:36:42 +0000

    update

--- rb101/lesson_2/refact_calc.rb
+++ rb101/lesson_2/refact_calc.rb
@@ -20,64 +20,71 @@ loop do
   else
     break
   end
-end 
+end
 
-loop do #main loop
+loop do # main loop
   num1 = ''
   num2 = ''
-  
-  loop do 
+
+  loop do
     prompt("What's the first number?")
     num1 = gets.chomp
     if valid_number?(num1)
       break
     else
       prompt("That doesn't look like a valid number...")
-    end 
-  end 
-  
-  loop do 
+    end
+  end
+
+  loop do
     prompt("What's the second number?")
     num2 = gets.chomp
     if valid_number?(num2)
       break
     else
       prompt("That doesn't look like a valid number...")
-    end 
+    end
   end
-  
+
   operator_prompt = <<-MSG
     What operation would you like to perform? 
     1) add 
     2) subtract 
     3) multiply 
     4) divide
-  MSG 
-  
+    MSG
+
   prompt(operator_prompt)
   operator = ''
-  
-  loop do 
+
+  loop do
     operator = gets.chomp
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Please enter a valid operation: 1) add 2) subtract 3) multiply 4) divide")
+      prompt(<<-MSG
+      Please enter a valid operation: 
+      1) add 
+      2) subtract 
+      3) multiply 4) 
+      divide"
+      MSG
+            )
     end
-  end 
-  
-  result = case operator 
+  end
+
+  result = case operator
            when '1' then num1.to_i + num2.to_i
            when '2' then num1.to_i - num2.to_i
            when '3' then num1.to_i * num2.to_i
            else          num1.to_f / num2.to_f
-           end 
-  
+           end
+
   prompt("The result is #{result}")
-  
+
   prompt("Would you like to perform another calculation? (Y to continue)")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-end 
+end
 
 prompt("Thank you for using calculator!")
