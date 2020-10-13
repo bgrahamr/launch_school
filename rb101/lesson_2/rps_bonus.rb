0797b82ab3714d231cd89bc5b15220a04a496c5f
Brad Graham <boosporch88888@gmail.com>
2020-10-13 17:10:39 +0000

    update

--- rb101/lesson_2/rps_bonus.rb
+++ rb101/lesson_2/rps_bonus.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'yaml'
 
 MESSAGES = YAML.load_file('rps_messages.yml')
-VALID_CHOICES = %w(r p s l sp)
+VALID_CHOICES = %w[r p s l sp].freeze
 
 def display(message, sleep_sec: 0, sys_clear: false)
   # Formats output messages, option to pause program for
@@ -12,17 +14,17 @@ def display(message, sleep_sec: 0, sys_clear: false)
   sleep(sleep_sec) if sleep_sec != 0
 end
 
-def show_greeting()
+def show_greeting
   display(MESSAGES['greeting'], sleep_sec: 3,
-          sys_clear: true)
+                                sys_clear: true)
 end
 
-def show_farewell()
+def show_farewell
   display(MESSAGES['farewell'], sleep_sec: 2,
-          sys_clear: true)
+                                sys_clear: true)
 end
 
-def show_rules()
+def show_rules
   loop do
     display(MESSAGES['rules'], sys_clear: true)
     escape = gets.chomp
@@ -56,28 +58,29 @@ def show_game_result(result)
   display(result, sleep_sec: 3)
 end
 
-def read_rules?()
+def read_rules?
   display(MESSAGES['explain_rules'], sys_clear: true)
   response = gets.chomp
-  %w( y yes).include?(response.downcase)
+  %w[y yes].include?(response.downcase)
 end
 
-def get_player_move()
+def player_picks_move
   # Solicits user input and returns validated input
   user_choice = ''
 
   loop do
     display(MESSAGES['move_prompt'])
-    user_choice = gets.chomp.downcase
 
+    user_choice = gets.chomp.downcase
     break if VALID_CHOICES.include?(user_choice)
+
     display(MESSAGES['error_prompt'], sleep_sec: 2)
   end
-  
+
   user_choice
 end
 
-def get_comp_move()
+def comp_picks_move
   VALID_CHOICES.sample
 end
 
@@ -86,19 +89,20 @@ def decide_round_result(player, computer)
   # 1 if computer wins, and nil if round is a tie
   # The trumps hash shows what a choice (key) trumps (value)
   trumps = {
-    'r'   => ['l', 's'],
-    'p'   => ['r', 'sp'],
-    's'   => ['l', 'p'],
-    'l'   => ['p', 'sp'],
-    'sp'  => ['r', 's']
+    'r' => %w[l s],
+    'p' => %w[r sp],
+    's' => %w[l p],
+    'l' => %w[p sp],
+    'sp' => %w[r s]
   }
 
   return if player == computer
+
   trumps[player].include?(computer) ? 0 : 1
 end
 
 def update_score(current_score, result)
-  current_score[result] += 1 unless result == nil
+  current_score[result] += 1 unless result.nil?
   current_score
 end
 
@@ -111,22 +115,23 @@ def decide_game_result(score_arr, points_to_win)
 end
 
 # Initiate game
-wins_needed = 3       # wins needed for best of 5
+wins_needed = 3       # wins neseded for best of 5
 score       = [0, 0]  # [player_score, computer_score]
 
-show_greeting()
-show_rules if read_rules?()
+show_greeting
+show_rules if read_rules?
 
-loop do   # Main loop
+# Main loop
+loop do
   show_scoreboard(score)
 
-  player_move = get_player_move()
+  player_move = player_picks_move
   show_player_move(player_move)
 
-  comp_move = get_comp_move()
+  comp_move = comp_picks_move
   show_comp_move(comp_move)
 
-  round_result  = decide_round_result(player_move, comp_move)
+  round_result = decide_round_result(player_move, comp_move)
   show_round_result(round_result)
 
   score = update_score(score, round_result)
@@ -137,4 +142,4 @@ game_result = decide_game_result(score, wins_needed)
 show_scoreboard(score)
 show_game_result(game_result)
 
-show_farewell()
+show_farewell
