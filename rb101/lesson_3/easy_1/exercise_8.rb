2a88810c4de6786dc3fb7f296f520bb6fd111d52
Brad Graham <boosporch88888@gmail.com>
2020-10-15 04:39:18 +0000

    update

---
+++ rb101/lesson_3/easy_1/exercise_8.rb
@@ -0,0 +1,10 @@
+# Given the hash below
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+# Turn this into an array containing only two elements: Barney's name and Barney's number
+
+flintstones = flintstones.assoc('Barney') 
+p flintstones
+
+# originally did .select .to_a .flatten; .assoc does all of that
